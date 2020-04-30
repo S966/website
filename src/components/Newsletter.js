e45a257abb6dead5dcc8cbbe6e93906b15f1a80f
Rami Taibah <taibah@gmail.com>
2020-05-01 02:15:08 +0300

    Add ConvertKit newsletter form

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -7,7 +7,7 @@ function Item({ src, alt, href }) {
       align="center"
       justifySelf="center"
       gridGap={10}
-      w="67%"
+      w="69%"
     >
       <Grid gridTemplateRows="auto auto">
         <Heading
@@ -26,59 +26,11 @@ function Item({ src, alt, href }) {
         </Text>
       </Grid>
       <Grid>
-        <div id="mc_embed_signup">
-          <form
-            action="http://currrva.us8.list-manage.com/subscribe/post?u=03baa6734b1ba1e29373d4fe4&amp;id=11d6d828b6"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            class="validate"
-            target="_blank"
-            novalidate
-          >
-            <div class="mc-field-group">
-              <label for="mce-FULLNAME">Full Name </label>
-              <input type="text" value="" name="FULLNAME" class="" id="mce-FULLNAME" />
-            </div>
-            <div class="mc-field-group">
-              <label for="mce-EMAIL">
-                Email Address <span class="asterisk">*</span>
-              </label>
-              <input
-                type="email"
-                value=""
-                name="EMAIL"
-                class="required email"
-                id="mce-EMAIL"
-              />
-            </div>
-            <div id="mce-responses" class="clear">
-              <div class="response" id="mce-error-response" style={{ display: 'none' }} />
-              <div
-                class="response"
-                id="mce-success-response"
-                style={{ display: 'none' }}
-              />
-            </div>
-            <div style={{ position: 'absolute', left: '-5000px' }}>
-              <input
-                type="text"
-                name="b_03baa6734b1ba1e29373d4fe4_11d6d828b6"
-                tabindex="-1"
-                value=""
-              />
-            </div>
-            <div class="clear">
-              <input
-                type="submit"
-                value="Subscribe"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                class="button"
-              />
-            </div>
-          </form>
-        </div>
+        <script
+          async
+          data-uid="3c9de99870"
+          src="https://966.ck.page/3c9de99870/index.js"
+        />
       </Grid>
     </Grid>
   )
@@ -86,7 +38,7 @@ function Item({ src, alt, href }) {
 
 export default function Newsletter() {
   return (
-    <Grid py="50px" background="#f4b860" w="100%">
+    <Grid py="30px" background="#f4b860" w="100%">
       <Item />
     </Grid>
   )
