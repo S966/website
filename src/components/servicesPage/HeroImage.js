3594e16c989bb5e3664f3deca464fb2f17d6be2a
Rami Taibah <taibah@gmail.com>
2020-05-07 04:11:46 +0300

    Optimizations

--- src/components/servicesPage/HeroImage.js
+++ src/components/servicesPage/HeroImage.js
@@ -1,27 +1,20 @@
-import { Carousel } from "react-responsive-carousel";
-import { Flex, Box, Image, Heading } from "@chakra-ui/core";
+import { Carousel } from 'react-responsive-carousel'
+import { Flex, Box, Image, Heading } from '@chakra-ui/core'
 
 function Item({ src, alt, text }) {
   return (
     <Box>
       <Image objectFit="cover" alt={alt} src={src} w="100%" />
-      <Heading
-        size="md"
-        textAlign="start"
-        pl="5"
-        py="2"
-        as="h1"
-        color="gray.100"
-      >
+      <Heading size="md" textAlign="start" pl="5" py="2" as="h1" color="gray.100">
         {text}
       </Heading>
     </Box>
-  );
+  )
 }
 
 export default function HeroImage() {
   return (
-    <Flex mt="5" w="95%">
+    <Flex mt="5" px="40px">
       <Carousel showStatus={false} showThumbs={false} showIndicators={false}>
         <Item
           alt="a wall filled with sticky notes about brainstorming ideas."
@@ -30,5 +23,5 @@ export default function HeroImage() {
         />
       </Carousel>
     </Flex>
-  );
+  )
 }
